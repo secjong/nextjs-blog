eb01cc8962b947d1d1502c4c31be1f4d574bcfc7
채세종 <sejong.chae@interpark.com>
2023-02-13 09:56:00 +0900

    샘플 수정

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,6 @@
 export default async function handler(req, res) {
   // 여기서 백엔드 api를 직접 쏜다?
-  const response = await fetch(
-    "https://api-gw.interparktour.com/rv/api/np/reservation/v1/card/code/mapng"
-  );
+  const response = await fetch("https://api.sampleapis.com/beers/ale");
   const data = await response.json();
   res.status(200).json(data);
   // .then((data) => {
